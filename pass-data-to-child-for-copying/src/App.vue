0f56d4684a5a8f8f4f3848a81647f29883469a24
Dean Agan <agandfr@gmail.com>
2021-04-13 20:23:08 +1000

    Adding test code

---
+++ pass-data-to-child-for-copying/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" :content="content"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created: function() {
+    this.content.name = "John";
+    this.content.number = "12345";
+  },
+  data: function() {
+    return {
+      content: {
+        name: "",
+        number: ""
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ pass-data-to-child-for-copying/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+   <b-container fluid>
+  <b-row class="my-1">
+    <b-col sm="3">
+      <label for="name">Name:</label>
+    </b-col>
+    <b-col sm="9">
+      <b-form-input id="name" v-model="data.name"></b-form-input>
+    </b-col>
+  </b-row>
+
+  <b-row class="my-1">
+    <b-col sm="3">
+      <label for="number">Number:</label>
+    </b-col>
+    <b-col sm="9">
+      <b-form-input id="number" v-model="data.number"></b-form-input>
+    </b-col>
+  </b-row>
+
+</b-container>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    content: Object
+  },
+  data: function() {
+    return {
+      data: { ...this.content }
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
